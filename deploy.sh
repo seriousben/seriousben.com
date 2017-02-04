ab2d6c48b752c783a9cf818d4e1ef2a02214c570
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2017-02-04 11:53:42 -0500

    Adding auto deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!env bash
+
+set -e
+shopt -s globstar
+
+for f in public/**; do
+  [ -f "$f" ] || continue
+  remoteFileName=${f:6}
+  echo "Uploading $f"
+  curl -F "$remoteFileName=@$f" "https://$NEOCITIES_USER:$NEOCITIES_PASS@neocities.org/api/upload"
+  echo "Uploaded $f"
+done;
